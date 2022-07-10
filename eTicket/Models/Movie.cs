8ba66fbb31774111313645b2629cf58ed45a6650
NilamPathak578 <pathaknilm@gmail.com>
2022-07-10 14:40:10 +0545

    Dbcontext Configuration

--- eTicket/Models/Movie.cs
+++ eTicket/Models/Movie.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using eTicket.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,15 @@ namespace eTicket.Models
 {
     public class Movie
     {
+        [Key]
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public double Price { get; set; }
+        public String ImageUrl { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime Enddate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
     }
 }
